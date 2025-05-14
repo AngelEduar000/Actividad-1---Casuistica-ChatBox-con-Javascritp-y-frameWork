7e1b6fc12ffdb260f3aded6704102d24b9cd24de
Sebastian Mayorga <smayorga5@udi.edu.co>
2025-05-14 10:35:14 -0500

    chatbot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ChatbotComponent },       // Ruta principal
+  { path: 'chatbot', component: ChatbotComponent } // Ruta específica opcional
+];
